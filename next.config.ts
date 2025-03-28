8127e298b4529d2742282c2fa6a4fbf29d9ae98d
satyam <satyam.dahlan@qure.ai>
2025-03-28 20:09:49 +0530

    build issue fixed

--- next.config.ts
+++ next.config.ts
@@ -4,9 +4,13 @@ const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
   cache: process.env.NODE_ENV === 'production',
-  staticPageGenerationTimeout: 0,
+  staticPageGenerationTimeout: 1000,
   output: 'standalone',
   generateEtags: false,
+  experimental: {
+    workerThreads: false,
+    cpus: 1
+  }
 };
 
 export default nextConfig;
